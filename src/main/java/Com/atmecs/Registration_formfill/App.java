8b95273511269a0c16eb6786aa8395f8e9d26802
Nithyashree-n <nithyashree.nithyanandam@atmecs.com>
2020-09-22 11:03:34 +0530

    registerfile

---
+++ src/main/java/Com/atmecs/Registration_formfill/App.java
@@ -0,0 +1,13 @@
+package Com.atmecs.Registration_formfill;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Com/atmecs/Registration_formfill/AppTest.java
@@ -0,0 +1,20 @@
+package Com.atmecs.Registration_formfill;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
